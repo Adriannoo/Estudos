a314e5360779edd188b5eb81df6902397281c493
Adriano Ramos Dos Santos Ramos <aramos@rafainpalace.corp>
2025-09-23 17:44:32 -0300

    terminando

--- 2_Semestre/mes2/prova/index.js
+++ 2_Semestre/mes2/prova/index.js
@@ -12,20 +12,19 @@ localStorage.setItem("pessoa", JSON.stringify(pessoa));
 // localStorage.getItem() - pega a string salva no local storage
 // JSON.parse - transforma de volta a string em objeto
 let dados = JSON.parse(localStorage.getItem("pessoa"));
+
+if (!dados) dados = 0; // Verifica se há dado salvo. Obrigatório!
+
 // Pego o dado salvo e transcrevo no <p> com ID "pessoa" com "innerText"
 document.getElementById("pessoa").innerText = dados.nome;
 
-const lista_objeto = [
-    {nome: "Carro", preco: 100000},
-    {nome: "Computador", preco: 53},
-    {nome: "Casa", preco: 100000}
-];
 
-lista_objeto.forEach(elements => {
-    document.getElementById("lista").innerHTML += `<li>${elements.nome} - ${elements.preco}</li>`;
-});
-
-let visitas = localStorage.getItem("visitas");
-if (!visitas) visitas = 0;
-visitas = parseInt(visitas) + 1;
-localStorage.setItem("visitas", visitas);
+fetch("produtos.json") // Faz uma requisição HTTP
+    .then(response => response.json()) // Converto eles para JSON
+    .then(lista_objeto => { // Recebe os dados já convertidos
+        // Percorre todo os elementos do objeto e transcreve dentro da lista ul, incrementando o li.
+        lista_objeto.forEach(elements => { 
+            document.getElementById("lista-local").innerHTML += `<li>${elements.nome} - ${elements.preco}</li>`;
+        });
+    })
+    .catch(error => console.error('Error', error));
