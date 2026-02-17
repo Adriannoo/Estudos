92281933b28ce4e62ba78875e119bacd6757c538
Adriano Ramos Dos Santos Ramos <aramos@rafainpalace.corp>
2026-02-16 23:32:19 -0300

    aprendendo bastante

--- 3_Semestre/SistemaGerencia/src/main/java/Funcionario.java
+++ 3_Semestre/SistemaGerencia/src/main/java/Funcionario.java
@@ -7,7 +7,10 @@ public abstract class Funcionario {
     public Funcionario(String nome, String cpf, double salarioBase) {
         this.nome = nome;
         this.cpf = cpf;
-        this.salarioBase = salarioBase;
+        //this.salarioBase = salarioBase;
+        // Utilizo o metodo de validacao no construtor para garantir que o salario base seja validado desde a criacao do objeto
+        // impedindo que um funcionario seja criado com um salario base negativo
+        setSalarioBase(salarioBase);
     }
 
     // Getters e Setters
@@ -30,6 +33,9 @@ public abstract class Funcionario {
     // Validação no Método para impedir salário base negativo
     public void setSalarioBase(double salarioBase) {
         if (salarioBase < 0) {
+            // Do pacote .lang, lanca uma excecao para indicar que o salario base nao pode ser negativo
+            // Ille joga o erro na tela, parando a execucao do programa
+            // "throw" - lança o erro; "IlleArgExc" - Argumento inválido
             throw new IllegalArgumentException("ERRO! Salário base não pode ser negativo!");
         }
         this.salarioBase = salarioBase;
@@ -39,4 +45,19 @@ public abstract class Funcionario {
         return salarioBase;
     }
 
+    // Metodo abstrato, que vai utilizar o polimorfismo para calcular o salario de cada tipo de funcionario
+    public abstract double calcularSalario();
+
+    public void aumentarSalario(double percentual) {
+        if (percentual < 0) {
+            throw new IllegalArgumentException("ERRO! Percentual de aumento não pode ser negativo!");
+        }
+        salarioBase += salarioBase * (percentual / 100);
+    }
+
+    public void exibirDados() {
+        System.out.print("-----------------------------\n");
+        System.out.printf("Nome: %s\nCPF: %s\nSalario Base: R$ %.2f\n Salario Final: R$ %.2f\n", nome, cpf, salarioBase, calcularSalario());
+        System.out.print("-----------------------------\n");
+    }
 }
--- 3_Semestre/SistemaGerencia/src/main/java/FuncionarioCLT.java
+++ 3_Semestre/SistemaGerencia/src/main/java/FuncionarioCLT.java
@@ -1,2 +1,14 @@
-public abstract class FuncionarioCLT {
+public class FuncionarioCLT extends Funcionario {
+    public FuncionarioCLT(String nome, String cpf, double salarioBase) {
+        // Chama o construtor da classe pai herdado da classe Funcionario com o metodo "super"
+        super(nome, cpf, salarioBase);
+    }
+
+    // Estou sobrescrevendo o metodo "pai" para calcular o salario do CLT
+    // Por isso da utilizacao da anotacao "@override" para indicar que estou sobrescrevendo um metodo da classe pai
+    // Criando uma nova implementacao
+    @Override
+    public double calcularSalario() {
+        return getSalarioBase();
+    }
 }
