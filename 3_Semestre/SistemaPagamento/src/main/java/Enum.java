90766fd2a80cf80b8e3ef74f1c3695ef8d2dd5cb
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-03-05 17:42:29 -0300

    coisa bizonha

---
+++ 3_Semestre/SistemaPagamento/src/main/java/Enum.java
@@ -0,0 +1,5 @@
+public enum Enum {
+    PIX,
+    CARTAO,
+    BOLETO
+}
---
+++ 3_Semestre/SistemaPagamento/src/main/java/PagamentoCartao.java
@@ -0,0 +1,11 @@
+
+public class PagamentoCartao implements Interface {
+
+    double valorCartao;
+    Enum tipoPagamento = Enum.CARTAO;
+
+    @Override
+    public void processarPagamento(double valor) {
+
+   }
+}
